5a5898c8ca467e4f1c1a1d9a9fae1a1ebfe3afb8
devude7 <dawidmrowczynski7@gmail.com>
2024-03-19 08:38:52 +0100

    initial structure

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
+from flask import Flask, redirect, render_template, url_for, request, session
 
+app = Flask(__name__)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+
+
+
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+@app.route("/")
+def index():
+    return render_template("index.html")
